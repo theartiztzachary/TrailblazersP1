7fc809c72cb613758e978e3c8b64a11c5068e2aa
vathsalav <vathsala.v@gmail.com>
2022-03-27 14:25:30 -0400

    need to test service layer

--- data_access_layer/employee_dao_imp.py
+++ data_access_layer/employee_dao_imp.py
@@ -7,12 +7,13 @@ class EmployeeDAOImp(EmployeeDAOInterface):
     def submit_reimbursement(self, reimbursements: ReimbursementData) -> ReimbursementData:
         sql = "insert into reimbursements values(default, %s, %s, %s, %s, %s) returning reimbursement_id"
         cursor = connection.cursor()
-        cursor.execute(sql, (reimbursements.employee_id, reimbursements.amount, reimbursements.reason, reimbursements.reimbursement_comment, reimbursements.status_code))
+        cursor.execute(sql, (
+        reimbursements.employee_id, reimbursements.amount, reimbursements.reason, reimbursements.reimbursement_comment,
+        reimbursements.status_code))
         connection.commit()
         returned_id = cursor.fetchone()[0]
         reimbursements.reimbursement_id = returned_id
         return reimbursements
-
 
     def get_reimbursement_info_by_id(self, reimbursement_id: int) -> ReimbursementData | None:
         sql = "select * from reimbursements where reimbursement_id = %s"
