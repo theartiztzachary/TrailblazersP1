9d59f2c1aac87d6c300224c7e3bdc7c76147c72c
Tashawn <tashawnhuggins@gmail.com>
2022-03-30 15:41:03 -0400

    modifying the api layer

--- api_layer/main.py
+++ api_layer/main.py
@@ -1,26 +1,22 @@
 from flask import Flask, request, jsonify
 
+from custom_exceptions.id_not_found import IdNotFound
+from data_access_layer.reimbursement_dao_imp import ReimbursementDAOImp
+from service_layer.reimbursement_service_imp import ReimbursementServiceImp
+
 app: Flask = Flask(__name__)
 
-
-@app.route("/json", methods=["GET"])
-def return_a_json():
-    customer_id = 1
-    first_name = "Ted"
-    last_name = "Teddington"
-
-    # because this will be converted to a JSON we should follow JSON naming conventions
-    customer_as_dictionary = {
-        "customerId": customer_id,
-        "firstName": first_name,
-        "lastName": last_name
-    }
-
-    # we use the jsonify method to convert our dictionary into a json
-    customer_as_json = jsonify(customer_as_dictionary)
-    return customer_as_json, 200
+reimbursement_dao = ReimbursementDAOImp()
+reimbursement_service = ReimbursementServiceImp(reimbursement_dao)
 
 
-@app.route("/cancel/employee_id/reimbursement_id", methods=["DELETE"])  # or /cancel/reimbursement_id"
+@app.route("/cancel/employee_id/reimbursement_id", methods=["PATCH"])  # or /cancel/reimbursement_id"
 def cancel_reimbursement_request():
-    pass
+    try:
+        pass
+    except IdNotFound as e:
+        error_message = {
+            "message": str(e)
+        }
+        error_json = jsonify(error_message)
+        return error_json, 404
