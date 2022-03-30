9e161bc0eb2b0c70744d2e79f9e42f5d4f40801d
AndraeyPompey <andraey.pompey@gmail.com>
2022-03-30 17:21:10 -0400

    api work

--- api_layer/main.py
+++ api_layer/main.py
@@ -1,16 +1,19 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
+
 from data_access_layer.dal_login_implementation import LogInDataAccessLayerImplementation
 from service_layer.serl_login_implementation import LogInServiceLayerImplementation
 from utilities.custom_exceptions.employee_not_found import EmployeeNotFound
 from utilities.custom_exceptions.incorrect_password import IncorrectPassword
 
 app: Flask = Flask(__name__)
+CORS(app)
 
 login_data_access_object = LogInDataAccessLayerImplementation()
 login_service = LogInServiceLayerImplementation(login_data_access_object)
 
 
-@app.route("/employee", methods=["GET"])
+@app.route("/employee", methods=["GET", "POST"])
 def select_employee_information():
     try:
         employee = request.get_json()
@@ -21,7 +24,8 @@ def select_employee_information():
             "result": result
         }
         result_json = jsonify(result_dictionary)
-        return result_json, 201
+        print("HELLO")
+        return result_json, 200, 201
 
     except EmployeeNotFound as e:
         message = {
