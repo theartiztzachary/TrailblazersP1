e4f1d6d4a6eea83fe837ab49da8bf33334cbef83
theartiztzachary <zacharygaming77@gmail.com>
2022-04-07 15:03:24 -0700

    :)?

--- zachs_branch/front_end_files/landing-page.js
+++
@@ -1,6 +0,0 @@
-const employeeID = document.getElementById("employee-id-test")
-
-function transferToHome() {
-    window.sessionStorage.setItem("employee-id", employeeID.value);
-    window.location.href = "employee-home-page.html"
-}
